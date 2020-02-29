3324a189792627cf2a39c3b4d83f8a7e0669e71f
Sebastien Hordeaux <shordeaux@waterproof.fr>
2020-02-29 14:15:13 +0100

    Toulouse

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-http   = require('http'),
-twilio = require('twilio'),
-client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN),
-port   = (process.env.PORT || 3000),
-url    = require('url');
+http = require('http'),
+    twilio = require('twilio'),
+    client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN),
+    port = (process.env.PORT || 3000),
+    url = require('url');
 
 http.createServer(function (req, res) {
   var query = url.parse(req.url, true).query;
-  console.dir(query);
+  //console.dir(query);
+  //console.log(query.path);
   var twiml = new twilio.TwimlResponse();
-  if(query.From === process.env.FRONT_DOOR_NUMBER){
+  console.log('Call from [' + query.From + '], expecting [' + process.env.FRONT_DOOR_NUMBER + ']');
+  if (query.From === process.env.FRONT_DOOR_NUMBER) {
     console.log('Greeting.');
-    twiml.say('Bienvenue à étincelle coworking. Première porte à gauche.', {
-      voice:'alice',
-      language:'fr-FR'
+    twiml.say('Bienvenue chez étincelle coworking.', {
+      voice: 'alice',
+      language: 'fr-FR'
     });
     console.log('Opening the door...');
     twiml.pause();
-    twiml.play({digits:'*'});
+    twiml.play({digits: '*'});
     twiml.pause();
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
+  }else{
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('File not found');
   }
 }).listen(port);
 
