d6bb00f8a53ea109014e47bc3521c06d66e99300
Sébastien Hordeaux <shordeaux@waterproof.fr>
2025-11-06 23:57:44 +0100

    .

--- app.js
+++ app.js
@@ -1,62 +1,59 @@
-http = require('http'),
-    https = require('https'),
-    twilio = require('twilio'),
-    client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN),
-    port = (process.env.PORT || 3000),
-    url = require('url');
+const https = require('https');
+const port = (process.env.PORT || 3000);
+const express = require('express');
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
-http.createServer(function (req, res) {
-//    var query = url.parse(req.url, true).query;
-//    console.dir(query);
-//    console.log(query.From);
-    var twiml = new twilio.TwimlResponse();
-//    console.log('Call from [' + query.From + '], expecting [' + process.env.FRONT_DOOR_NUMBER + ']');
-//    if (query.From === process.env.FRONT_DOOR_NUMBER) {
-        console.log('Greeting.');
-        twiml.say('Bienvenue chez étincelle coworking.', {
-            voice: 'alice',
-            language: 'fr-FR'
+const app = express();
+
+// Create a route that will handle Twilio webhook requests, sent as an
+// HTTP POST to /voice in our application
+app.post('/', (request, response) => {
+    // Use the Twilio Node.js SDK to build an XML response
+    const twiml = new VoiceResponse();
+    console.log('Greeting.');
+    twiml.say('Bienvenue chez étincelle coworking.', {
+        voice: 'alice',
+        language: 'fr-FR'
+    });
+
+
+    https.get(process.env.DOOR_STATUS_API_URL, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
         });
 
-        https.get(process.env.DOOR_STATUS_API_URL, (resp) => {
-            let data = '';
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            if (data === 'Yes') {
+                console.log('Opening the door...');
+                twiml.pause();
+                twiml.play({digits: '*'});
+                twiml.pause();
+            } else {
+                console.log('Opening the door...');
+                twiml.say('Nos bureaux sont actuellement fermés.', {
+                    voice: 'alice',
+                    language: 'fr-FR'
+                });
 
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
+            }
+            response.type('text/xml');
+            response.send(twiml.toString());
 
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                if (data === 'Yes') {
-                    console.log('Opening the door...');
-                    twiml.pause();
-                    twiml.play({digits: '*'});
-                    twiml.pause();
-
-                    res.writeHead(200, {'Content-Type': 'text/xml'});
-                    res.end(twiml.toString());
-                } else {
-                    console.log('Opening the door...');
-                    twiml.say('Nos bureaux sont actuellement fermés.', {
-                        voice: 'alice',
-                        language: 'fr-FR'
-                    });
-
-                    res.writeHead(200, {'Content-Type': 'text/xml'});
-                    res.end(twiml.toString());
-                }
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Internal error');
         });
-//    } else {
-//        res.writeHead(404, {'Content-Type': 'text/plain'});
-//        res.end('File not found');
-//    }
-}).listen(port);
 
-console.log('TwiML servin\' server running on PORT: ' + port);
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+        response.type('text/plain');
+        response.send('Internal error');
+    });
+});
+
+// Create an HTTP server and listen for requests on port 1337
+app.listen(port, () => {
+    console.log('TwiML server running on port ' + port);
+});
+
