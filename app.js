7c4ee0b243d3228f940e63233513ea5f963f0107
Sébastien Hordeaux <shordeaux@waterproof.fr>
2025-11-07 02:09:17 +0100

    .

--- app.js
+++ app.js
@@ -1,65 +1,28 @@
-const http = require('http');
-const https = require('https');
-//const twilio = require('twilio');
-//client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN),
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-const port = (process.env.PORT || 3000);
-const url = require('url');
+// Utilisée comme Fonction chez Twilio directement https://console.twilio.com/us1/develop/functions/services
+const request = require('sync-request');
 
-http.createServer(function (request, response) {
-    var query = url.parse(request.url, true).query;
-    console.dir(query);
-    console.log(query.From);
-    const twiml = new VoiceResponse();
-    console.log('Call from [' + query.From + '], expecting [' + process.env.FRONT_DOOR_NUMBER + ']');
-    //if (query.From === process.env.FRONT_DOOR_NUMBER) {
-        console.log('Greeting.');
-        twiml.say('Bienvenue chez étincelle coworking.', {
-            voice: 'alice',
+exports.handler = function(context, event, callback) {
+    let twiml = new Twilio.twiml.VoiceResponse();
+
+    const lookupResponse = request('GET', 'https://etincelle.at/api/intercom/toulouse-carmes');
+
+    console.log(lookupResponse.getBody().toString());
+    if(lookupResponse.getBody().toString() === 'Yes'){
+        twiml.say({
+            voice: 'Google.fr-FR-Chirp3-HD-Aoede',
             language: 'fr-FR'
-        });
+        }, 'Bienvenue chez Etincelle Coworking. Poussez le battant de gauche et montez au premier étage.');
+        console.log('Opening the door...');
+        twiml.pause();
+        twiml.play({digits: '*'});
+        twiml.pause();
+    }else{
+        console.log('Leaving door closed...');
+        twiml.say({
+            voice: 'Google.fr-FR-Chirp3-HD-Aoede',
+            language: 'fr-FR'
+        }, 'Nos bureaux sont actuellement fermés.');
+    }
 
-        https.get(process.env.DOOR_STATUS_API_URL, (resp) => {
-            let data = '';
-
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                if (data === 'Yes') {
-                    console.log('Opening the door...');
-                    twiml.pause();
-                    twiml.play({digits: '*'});
-                    twiml.pause();
-
-                    response.type('text/xml');
-                    response.send(twiml.toString());
-                } else {
-                    console.log('Opening the door...');
-                    twiml.say('Nos bureaux sont actuellement fermés.', {
-                        voice: 'alice',
-                        language: 'fr-FR'
-                    });
-
-                    response.type('text/xml');
-                    response.send(twiml.toString());
-                }
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-            response.type('text/plain');
-            response.send('Internal error');
-        });
-    //} else {
-//        res.writeHead(404, {'Content-Type': 'text/plain'});
-//        res.end('File not found');
-    //}
-}).listen(port);
-
-console.log('TwiML servin\' server running on PORT: ' + port);
-
-
+    return callback(null, twiml);
+};
