a129b86ea55e979e0aaff80ed8566190335ef87d
Sébastien Hordeaux <shordeaux@waterproof.fr>
2025-11-07 15:44:40 +0100

    .

--- app.js
+++ app.js
@@ -3,25 +3,22 @@ const request = require('sync-request');
 
 exports.handler = function(context, event, callback) {
     let twiml = new Twilio.twiml.VoiceResponse();
+    const voice_config = {
+        voice: 'Google.fr-FR-Chirp3-HD-Aoede',
+        language: 'fr-FR'
+    };
 
     const lookupResponse = request('GET', 'https://etincelle.at/api/intercom/toulouse-carmes');
 
-    console.log(lookupResponse.getBody().toString());
     if(lookupResponse.getBody().toString() === 'Yes'){
-        twiml.say({
-            voice: 'Google.fr-FR-Chirp3-HD-Aoede',
-            language: 'fr-FR'
-        }, 'Bienvenue chez étincelle coworking. Nos bureaux sont situés au premier étage.');
+        twiml.say(voice_config, 'Bienvenue chez étincelle coworking. Nos bureaux sont situés au premier étage.');
         console.log('Opening the door...');
         twiml.pause();
         twiml.play({digits: '*'});
         twiml.pause();
     }else{
         console.log('Leaving door closed...');
-        twiml.say({
-            voice: 'Google.fr-FR-Chirp3-HD-Aoede',
-            language: 'fr-FR'
-        }, 'Nos bureaux sont actuellement fermés.');
+        twiml.say(voice_config, 'Nos bureaux sont actuellement fermés.');
     }
 
     return callback(null, twiml);
